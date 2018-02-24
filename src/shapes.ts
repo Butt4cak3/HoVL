d9497b5731f2b620fdd54e44daee85f2aa98ce2c
Gustorn <gustorn@gmail.com>
2018-02-24 20:29:34 +0100

    Restore circle center/radius

--- src/shapes.ts
+++ src/shapes.ts
@@ -10,15 +10,15 @@ interface Trs {
 export abstract class Shape {
   public color: string;
 
-  protected translation: Vector;
-  protected rotation: number;
-  protected scale: Vector;
+  public translation: Vector;
+  public rotation: number;
+  public scaling: Vector;
 
   constructor(color: string, trs: Partial<Trs>) {
     this.color = color;
     this.translation = trs.translation || new Vector(0, 0);
     this.rotation = trs.rotation || 0;
-    this.scale = typeof trs.scale === "number"
+    this.scaling = typeof trs.scale === "number"
       ? new Vector(trs.scale, trs.scale)
       : trs.scale || new Vector(1, 1);
   }
@@ -27,7 +27,7 @@ export abstract class Shape {
     scene.context.save();
     scene.context.translate(this.translation.x, this.translation.y);
     scene.context.rotate(this.rotation);
-    scene.context.scale(this.scale.x, this.scale.y);
+    scene.context.scale(this.scaling.x, this.scaling.y);
     this.drawInternal(scene);
     scene.context.restore();
   }
@@ -36,27 +36,20 @@ export abstract class Shape {
 }
 
 export class Circle extends Shape {
-  public get center() {
-    return this.translation;
-  }
-
-  public get radius() {
-    return this.scale.x;
-  }
-
-  public set radius(value: number) {
-    this.scale = new Vector(value, value);
-  }
+  public center: Vector;
+  public radius: number;
 
   constructor(x: number, y: number, radius: number, color: string) {
-    super(color, { translation: new Vector(x, y), scale: radius });
+    super(color, {});
+    this.center = new Vector(x, y);
+    this.radius = radius;
   }
 
   protected drawInternal(scene: Scene): void {
     const ctx = scene.context;
 
     ctx.beginPath();
-    ctx.arc(0, 0, 1, 0, Math.PI * 2);
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
