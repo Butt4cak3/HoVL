d5aa2303a07802a97f2925bd03c393f500255be4
Gustorn <gustorn@gmail.com>
2018-02-25 19:44:04 +0100

    Nicer object copy in merge function

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ export function mergeWithoutUndefined<T extends { [key: string]: any }>(
   base: T,
   ...overrides: Array<Partial<T>>
 ): T {
-  const result = { ...(base as object) } as T;
+  const result = Object.assign({}, base);
   for (const override of overrides) {
     for (const [key, value] of Object.entries(override)) {
       if (value != null) {
