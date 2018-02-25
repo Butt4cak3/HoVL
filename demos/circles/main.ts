4ad225ab0707381e11d6f002b9d33e6b35fce28f
Gustorn <gustorn@gmail.com>
2018-02-25 14:59:39 +0100

    Use native canvas transforms (#6)

--- demos/circles/main.ts
+++ demos/circles/main.ts
@@ -12,8 +12,8 @@ class CirclesScene extends Scene {
   }
 
   protected update(time: number): void {
-    this.shapes[0].pos.x = 5 + Math.sin(time) * 3;
-    this.shapes[1].pos.y = 5 + Math.sin(time) * 3;
+    this.shapes[0].translation.x = 5 + Math.sin(time) * 3;
+    this.shapes[1].translation.y = 5 + Math.sin(time) * 3;
   }
 }
 
--- src/shapes.ts
+++ src/shapes.ts
@@ -1,34 +1,58 @@
-import { Scene } from "hovl/scene";
 import { Vector } from "hovl/vector";
 
+interface Trs {
+  translation: Vector;
+  rotation: number;
+  scale: Vector | number;
+}
+
 export abstract class Shape {
-  public pos: Vector;
   public color: string;
 
-  constructor(x: number, y: number, color: string) {
-    this.pos = new Vector(x, y);
+  public translation: Vector;
+  public rotation: number;
+  public scale: Vector;
+
+  constructor(
+    color: string,
+    {
+      translation = new Vector(0, 0),
+      rotation = 0,
+      scale = new Vector(1, 1)
+    }: Partial<Trs>
+  ) {
     this.color = color;
+
+    this.translation = translation.copy();
+    this.rotation = rotation;
+    this.scale =
+      typeof scale === "number" ? new Vector(scale, scale) : scale.copy();
   }
 
-  public abstract draw(scene: Scene): void;
+  public draw(context: CanvasRenderingContext2D): void {
+    context.save();
+    context.translate(this.translation.x, this.translation.y);
+    context.rotate(this.rotation);
+    context.scale(this.scale.x, this.scale.y);
+    this.drawInternal(context);
+    context.restore();
+  }
+
+  protected abstract drawInternal(context: CanvasRenderingContext2D): void;
 }
 
 export class Circle extends Shape {
   public radius: number;
 
   constructor(x: number, y: number, radius: number, color: string) {
-    super(x, y, color);
+    super(color, { translation: new Vector(x, y) });
     this.radius = radius;
   }
 
-  public draw(scene: Scene): void {
-    const { x, y } = scene.toCanvasSpace(this.pos);
-    const radius = scene.toCanvasSpace(this.radius);
-    const ctx = scene.context;
-
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
+  protected drawInternal(context: CanvasRenderingContext2D): void {
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fillStyle = this.color;
+    context.fill();
   }
 }
