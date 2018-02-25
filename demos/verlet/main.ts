ef2855b371ead1df2ac9f6a9bf7f67d40398e4a6
Marius Becker <marius.becker.8@gmail.com>
2018-02-25 16:50:02 +0100

    Made the box an actual box and added comments

--- demos/verlet/main.ts
+++ demos/verlet/main.ts
@@ -1,6 +1,6 @@
 import { Scene } from "hovl/scene";
 import { Line, Circle } from "hovl/shapes";
-import { Vector } from "hovl/vector";
+import { Vector, add } from "hovl/vector";
 
 const BOX_X = 1;
 const BOX_Y = 1;
@@ -8,6 +8,7 @@ const BOX_WIDTH = 10;
 const BOX_HEIGHT = 10;
 const GRAVITY = 9.81;
 
+// Velocity Verlet
 function verletPos(x: number, v: number, a: number, dt: number): number {
   return x + v * dt + 0.5 * a * dt * dt;
 }
@@ -16,6 +17,7 @@ function verletVel(v: number, a: number, dt: number): number {
   return v + a * dt;
 }
 
+// We need a circle that knows its velocity and bounciness
 class Ball extends Circle {
   public vel: Vector = Vector(0, 0);
   public bounciness = 0.8;
@@ -27,31 +29,66 @@ class VerletScene extends Scene {
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
 
+    // These lines create a bounding box that the ball will bounce off of
     this.shapes.push(
       new Line(Vector(0, 0), Vector(BOX_WIDTH, 0), 0.1, "#000000", {
+        translate: Vector(BOX_X, BOX_Y)
+      }),
+      new Line(Vector(0, 0), Vector(BOX_WIDTH, 0), 0.1, "#000000", {
         translate: Vector(BOX_X, BOX_Y + BOX_HEIGHT)
+      }),
+      new Line(Vector(0, 0), Vector(0, BOX_HEIGHT), 0.1, "#000000", {
+        translate: Vector(BOX_X, BOX_Y)
+      }),
+      new Line(Vector(0, 0), Vector(0, BOX_HEIGHT), 0.1, "#000000", {
+        translate: Vector(BOX_X + BOX_WIDTH, BOX_Y)
       })
     );
 
+    // Create a ball a bit above the center of the box
     this.ball = new Ball(0.5, "#00AA77", {
       translate: Vector(BOX_X + (BOX_WIDTH / 2), BOX_Y + 2)
     });
 
+    // Give it a random starting velocity
+    this.ball.vel = add(this.ball.vel, Vector(Math.random() * 40 - 20, Math.random() * 40 - 20));
+
+    // Add the ball to the shapes so that it gets drawn
     this.shapes.push(this.ball);
   }
 
   protected update(_: number, dt: number): void {
     const ball = this.ball;
 
+    // Calculate the new position
     ball.translate.x = verletPos(ball.translate.x, ball.vel.x, 0, dt);
     ball.translate.y = verletPos(ball.translate.y, ball.vel.y, GRAVITY, dt);
 
+    // Calculate the new velocity
     ball.vel.x = verletVel(ball.vel.x, 0, dt);
     ball.vel.y = verletVel(ball.vel.y, GRAVITY, dt);
 
-    if (ball.translate.y > BOX_Y + BOX_HEIGHT - ball.radius) {
-      ball.translate.y = BOX_Y + BOX_HEIGHT - ball.radius;
+    // These are the coordinates at which the ball hits a wall
+    const boundTop = BOX_Y + ball.radius;
+    const boundBottom = BOX_Y + BOX_HEIGHT - ball.radius;
+    const boundLeft = BOX_X + ball.radius;
+    const boundRight = BOX_X + BOX_WIDTH - ball.radius;
+
+    // Check for and resolve collisions
+    if (ball.translate.y > boundBottom) {
+      ball.translate.y = boundBottom;
       ball.vel.y *= -ball.bounciness;
+    } else if (ball.translate.y < boundTop) {
+      ball.translate.y = boundTop;
+      ball.vel.y *= -ball.bounciness;
+    }
+
+    if (ball.translate.x > boundRight) {
+      ball.translate.x = boundRight;
+      ball.vel.x *= -ball.bounciness;
+    } else if (ball.translate.x < boundLeft) {
+      ball.translate.x = boundLeft;
+      ball.vel.x *= -ball.bounciness;
     }
   }
 }
